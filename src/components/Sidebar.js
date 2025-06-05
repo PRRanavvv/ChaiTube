e06da04fa9a8dcc4cad4418a83e4233c826a6486
Pranav Rana <ranapranav333@gmail.com>
2025-06-05 17:41:57 +0530

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
-  const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
+  const isMenuOpen = useSelector((store) => store.app.isMenuOpen)
 
   return (
     <div
